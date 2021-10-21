ef82939f4c5acd123bc97f9e564da7e07f6aa8ea
FernandoLuc4s <fernandolucas2002@gmail.com>
2021-10-20 23:42:07 -0300

    atualizando os exerciciosresolvidos2

---
+++ Seção 9 Enumerações e composição/Enumerações e composições/ExercicioResolvido2/Entities/Post.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioResolvido2.Entities
+{
+    class Post
+    {
+        public DateTime Moment { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public int Likes { get; set; }
+        public List<Comment> Comments { get; set; } = new List<Comment>();
+
+        public Post()
+        {
+        }
+
+        public Post(DateTime moment, string title, string content, int likes)
+        {
+            Moment = moment;
+            Title = title;
+            Content = content;
+            Likes = likes;
+        }
+
+        public void AddComment(Comment comment) 
+        {
+            Comments.Add(comment); // adicionando comentario na lista
+        }
+        public void RemoveComment(Comment comment)
+        {
+            Comments.Remove(comment); // removendo comentario na lista
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine(Title); // imprime a linha e logo após a quebra
+            sb.Append(Likes ); //somente imprime
+            sb.Append(" Likes - ");
+            sb.AppendLine(Moment.ToString("dd/MM/yyyy HH:mm:ss"));
+            sb.AppendLine(Content);
+            sb.AppendLine("Comments: ");
+           
+            foreach(Comment c in Comments)
+            {
+                sb.AppendLine(c.Text);
+            }
+            return sb.ToString();
+        }
+    }
+}
