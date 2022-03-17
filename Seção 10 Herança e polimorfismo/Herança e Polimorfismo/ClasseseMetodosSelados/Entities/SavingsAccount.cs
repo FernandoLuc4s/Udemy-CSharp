50bb2083971875408da45003b6e8c61ddca05d30
FernandoLuc4s <fernandolucas2002@gmail.com>
2022-03-17 16:53:06 -0300

    Classes e métodos selados

---
+++ Seção 10 Herança e polimorfismo/Herança e Polimorfismo/ClasseseMetodosSelados/Entities/SavingsAccount.cs
@@ -0,0 +1,28 @@
+﻿
+namespace  ClasseseMetodosSelados.Entities
+{
+     class SavingsAccount : Account // sealed = classe selada, isso significa que a classe não pode ser herdada
+    {
+        public double InterestRate { get; set; }
+
+        public SavingsAccount()
+        {
+        }
+
+        public SavingsAccount(int number, string holder, double balance, double interestRate)
+            :base(number, holder, balance) // reaproveitando construtor
+        {
+            InterestRate = interestRate;
+        }
+
+        public void UpdateBalance() {
+            Balance += Balance * InterestRate;
+        }
+
+        public sealed override void Withdraw(double amount) // override = sobrescrita de metodo, sealed = indica que o metodo não pode ser sobreescrito novamente
+        {
+            base.Withdraw(amount); // a palavra base permite que você pegue o mesmo metódo da superclasse e depois implemente algo usando a sobreposição
+            Balance -= 2.0;
+        }
+    }
+}
---
+++ Seção 10 Herança e polimorfismo/Herança e Polimorfismo/ClasseseMetodosSelados/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ClasseseMetodosSelados
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
