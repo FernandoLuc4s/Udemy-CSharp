4aa001db6acc60d4ecd2e38065ef3f63971128d6
FernandoLuc4s <fernandolucas2002@gmail.com>
2022-03-17 08:12:56 -0300

    Sobreposição, palavras virtual, override e base

---
+++ Seção 10 Herança e polimorfismo/Herança e Polimorfismo/SobreposiçãoPalavrasVirtualOverrideeBase/Entities/Account.cs
@@ -0,0 +1,28 @@
+﻿namespace SobreposiçãoPalavrasVirtualOverrideeBase.Entities
+{
+    class Account
+    {
+        public int Number { get; private set; }
+        public string Holder { get; private set; }
+        public double Balance { get; protected set; }
+
+        public Account()
+        {
+        }
+
+        public Account(int number, string holder, double balance)
+        {
+            Number = number;
+            Holder = holder;
+            Balance = balance;
+        }
+
+        public virtual void Withdraw(double amount) { // virtual = metodo pode ser sobrescrito ou sobreposto nas subclasses
+            Balance -= amount +5.0; // incluindo taxa de conta comum
+        }
+        public void Deposit(double amount)
+        {
+            Balance += amount;
+        }
+    }
+}
---
+++ Seção 10 Herança e polimorfismo/Herança e Polimorfismo/SobreposiçãoPalavrasVirtualOverrideeBase/Entities/SavingsAccount.cs
@@ -0,0 +1,28 @@
+﻿
+namespace SobreposiçãoPalavrasVirtualOverrideeBase.Entities
+{
+    class SavingsAccount : Account
+    {
+        public double InterestRate { get; set; }
+
+        public SavingsAccount()
+        {
+        }
+
+        public SavingsAccount(int number, string holder, double balance, double interestRate)
+            :base(number, holder, balance) // reaproveitando construtor
+        {
+            InterestRate = interestRate;
+        }
+
+        public void UpdateBalance() {
+            Balance += Balance * InterestRate;
+        }
+
+        public override void Withdraw(double amount) // override = sobrescrita de metodo
+        {
+            base.Withdraw(amount); // a palavra base permite que você pegue o mesmo metódo da superclasse e depois implemente algo usando a sobreposição
+            Balance -= 2.0;
+        }
+    }
+}
