5a34ee27c45e01b2baf46263f54d79f7e39ea7f3
FernandoLuc4s <fernandolucas2002@gmail.com>
2022-03-20 10:54:39 -0300

    Tratamento de Exceções e estrutra try-catch

---
+++ Seção 11 Tratamento de Exceções/Tratamento de Exceções/EstruturaTry-Catch/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace EstruturaTry_Catch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try // = tenta executar esse código
+            {
+                int n1 = int.Parse(Console.ReadLine());
+                int n2 = int.Parse(Console.ReadLine());
+
+                int result = n1 / n2;
+
+                Console.WriteLine(result);
+            }
+            catch (DivideByZeroException) // = se ocorrer alguma excessão  
+            {
+                Console.WriteLine("Division by zero is not allowed");
+            }
+            catch (FormatException e) 
+            {
+                Console.WriteLine("Format error! "+e.Message);
+            }
+
+        }
+    }
+}
