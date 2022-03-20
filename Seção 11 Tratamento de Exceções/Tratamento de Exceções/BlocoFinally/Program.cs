ea877903f700917c1e273e285665c0b76a3f91b4
FernandoLuc4s <fernandolucas2002@gmail.com>
2022-03-20 11:01:03 -0300

    Bloco Finally

---
+++ Seção 11 Tratamento de Exceções/Tratamento de Exceções/BlocoFinally/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+namespace BlocoFinally
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileStream fs = null;
+            try
+            {
+                fs = new FileStream(@"C:\temp\data.txt", FileMode.Open);
+                StreamReader sr = new StreamReader(fs);
+                string line = sr.ReadLine();
+                Console.WriteLine(line);
+            }
+            catch (FileNotFoundException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally // testa se está e aberto e fecha
+            {
+                if (fs != null)
+                {
+                    fs.Close();
+                }
+            }
+
+        }
+    }
+}
