6b5a57ad22867d35c73267ef589949fd843b1380
FernandoLuc4s <fernandolucas2002@gmail.com>
2022-03-21 17:02:59 -0300

    Criando exceções personalizadas - PARTE 1, exceções criadas de forma errada propositalmente para ensino

---
+++ Seção 11 Tratamento de Exceções/Tratamento de Exceções/CriandoExcecoesPersonalizadas/Entities/Reservation.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+
+namespace CriandoExcecoesPersonalizadas.Entities
+{
+    class Reservation
+    {
+        public int RoomNumber { get; set; }
+        public DateTime CheckIn { get; set; }
+        public DateTime ChekOut { get; set; }
+
+        public Reservation()
+        {
+        }
+
+        public Reservation(int roomNumber, DateTime checkin, DateTime chekout)
+        {
+            RoomNumber = roomNumber;
+            CheckIn = checkin;
+            ChekOut = chekout;
+        }
+
+        public int Duration() 
+        {
+            TimeSpan duration = ChekOut.Subtract(CheckIn); // pegar a diferença entre um instante e outro
+            return (int)duration.TotalDays; // retorna o total de dias
+        }
+
+        public void UpadateDates(DateTime checkIn, DateTime checkOut) {
+            CheckIn = checkIn;
+            ChekOut = checkOut;
+        }
+
+        public override string ToString()
+        {
+            return "Room "
+                + RoomNumber
+                + ", check-in: "
+                + CheckIn.ToString("dd/MM/yyyy")
+                + ", check-out: "
+                + ChekOut.ToString("dd/MM/yyyy")
+                +", "
+                +Duration()
+                +" nights";
+
+        }
+    }
+}
---
+++ Seção 11 Tratamento de Exceções/Tratamento de Exceções/CriandoExcecoesPersonalizadas/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using CriandoExcecoesPersonalizadas.Entities;
+
+namespace CriandoExcecoesPersonalizadas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Room number: ");
+            int number = int.Parse(Console.ReadLine());
+            Console.Write("Check-in date (dd/MM/yyyy): ");
+            DateTime checkIn = DateTime.Parse(Console.ReadLine());
+            Console.Write("Check-out date (dd/MM/yyyy): ");
+            DateTime checkOut = DateTime.Parse(Console.ReadLine());
+
+            // forma errada de verificação por conta da delegação e também dos if's e else's
+
+            if (checkOut <= checkIn)
+            {
+                Console.WriteLine("Erros in reservation: Check-out date must be after check-in date");
+            }
+            else
+            {
+                Reservation reservation = new Reservation(number, checkIn, checkOut);
+                Console.WriteLine("Reservation: "+reservation);
+                Console.WriteLine();
+                Console.WriteLine("Enter data to update tha reservation:");
+                Console.Write("Check-in date (dd/MM/yyyy): ");
+                checkIn = DateTime.Parse(Console.ReadLine());
+                Console.Write("Check-out date (dd/MM/yyyy): ");
+                checkOut = DateTime.Parse(Console.ReadLine());
+
+                DateTime now = DateTime.Now;
+
+                if (checkIn < now || checkOut < now)
+                {
+                    Console.WriteLine("Error in reservation: reservations dates for update must be future dates");
+                }
+                else if (checkOut <= checkIn)
+                {
+                    Console.WriteLine("Erros in reservation: Check-out date must be after check-in date");
+                }
+                else 
+                {
+                    reservation.UpadateDates(checkIn, checkOut);
+                    Console.WriteLine("Reservation: "+reservation);
+                }
+            }
+            
+        }
+    }
+}
