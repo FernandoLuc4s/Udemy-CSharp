dbc8c2cf01028e58cc97806f090d83e70aaa1476
FernandoLuc4s <fernandolucas2002@gmail.com>
2022-03-21 23:59:41 -0300

    Exercício proposto (exceções)

---
+++ Seção 11 Tratamento de Exceções/Tratamento de Exceções/ExercProposto(Excecoes)/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Globalization;
+using ExercProposto_Excecoes_.Entities;
+using ExercProposto_Excecoes_.Entities.Exceptions;
+
+namespace ExercProposto_Excecoes_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter account data");
+                Console.Write("Number: ");
+                int number = int.Parse(Console.ReadLine());
+                Console.Write("Holder: ");
+                string holder = Console.ReadLine();
+                Console.Write("Initial balance: ");
+                double balance = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.Write("Withdraw limit: ");
+                double withDrawLimit = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                Account account = new Account(number, holder, balance, withDrawLimit);
+
+                Console.WriteLine();
+
+                Console.Write("Enter amount for withdraw: ");
+                double amount = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                account.WithDraw(amount);
+
+                Console.WriteLine(account);
+            }
+            catch (DomainException e) 
+            {
+                Console.WriteLine("Withdraw error: "+e.Message);
+            }
+        }
+    }
+}
