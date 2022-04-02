bce5ab2af9ec6a366bdecd3e69e6cec7324f148a
FernandoLuc4s <fernandolucas2002@gmail.com>
2022-04-01 22:36:49 -0300

    FileStream e StreamReader

---
+++ Seção 13 Trabalhando com arquivos/Trabalhando com arquivos/FileStreamEStreamReader/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+
+namespace FileStreamEStreamReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\User\OneDrive\Área de Trabalho\udemy\C#\Seção 13 Trabalhando com arquivos\testes\file1.txt";
+          
+            StreamReader sr = null;
+            try
+            {
+
+
+                sr = File.OpenText(path); // usando o filestram de uma forma implicita
+                while (!sr.EndOfStream) { // enquanto não chegar no fim da stream
+                    string line = sr.ReadLine(); // ler uma linha do arquivo
+
+                    Console.WriteLine(line);
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error accurred");
+                Console.WriteLine(e.Message);
+            }
+            finally {
+                if (sr != null) sr.Close();
+              
+            }
+        }
+    }
+}
