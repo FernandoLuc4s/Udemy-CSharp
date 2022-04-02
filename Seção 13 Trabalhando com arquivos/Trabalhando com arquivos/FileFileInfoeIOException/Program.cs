7900efdf558dce22b4af845959a17517dad03f0c
FernandoLuc4s <fernandolucas2002@gmail.com>
2022-04-01 22:09:09 -0300

    File, FileInfo e IOException

---
+++ Seção 13 Trabalhando com arquivos/Trabalhando com arquivos/FileFileInfoeIOException/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace FileFileInfoeIOException
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourcePath = @"C:\Users\User\OneDrive\Área de Trabalho\udemy\C#\Seção 13 Trabalhando com arquivos\testes\file1.txt";
+            string targetPath = @"C:\Users\User\OneDrive\Área de Trabalho\udemy\C#\Seção 13 Trabalhando com arquivos\testes\file2.txt";
+
+            try
+            {
+                FileInfo fileInfo = new FileInfo(sourcePath);
+                fileInfo.CopyTo(targetPath);
+                string[] lines = File.ReadAllLines(sourcePath); // ler todas as linhas e guardar
+
+                foreach (string line in lines)
+                {
+                    Console.WriteLine(line);
+                }
+               
+            }
+            catch (IOException e) {
+                Console.WriteLine("An erros ocurred");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
