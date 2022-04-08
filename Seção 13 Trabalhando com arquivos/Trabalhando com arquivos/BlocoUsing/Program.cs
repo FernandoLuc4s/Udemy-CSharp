608ef6eb21018ef7043c1f07bed291980d006de0
FernandoLuc4s <fernandolucas2002@gmail.com>
2022-04-08 20:27:04 -0300

    Bloco using

---
+++ Seção 13 Trabalhando com arquivos/Trabalhando com arquivos/BlocoUsing/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace BlocoUsing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string path = @"C:\Users\User\OneDrive\Área de Trabalho\udemy\C#\Seção 13 Trabalhando com arquivos\testes\file1.txt";
+                // using =  fecha bloco após ser executado
+                using (StreamReader sr =  File.OpenText(path))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string line = sr.ReadLine();
+                        Console.WriteLine(line);
+                    }
+                }
+
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error ocurred");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
