5ff997fb61723a5b5a0d3d37ce3e2f6aa3d796ae
FernandoLuc4s <fernandolucas2002@gmail.com>
2022-04-12 18:33:49 -0300

    Stream Writer

---
+++ Seção 13 Trabalhando com arquivos/Trabalhando com arquivos/StreamWriter/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+namespace AulaStreamWriter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourcePath = @"C:\Users\User\OneDrive\Área de Trabalho\udemy\C#\Seção 13 Trabalhando com arquivos\testes\file1.txt";
+            string targetPath = @"C:\Users\User\OneDrive\Área de Trabalho\udemy\C#\Seção 13 Trabalhando com arquivos\testes\file2.txt";
+
+            try
+            {
+                string[] lines = File.ReadAllLines(sourcePath);
+
+                using ( StreamWriter sw = File.AppendText(targetPath)) {
+                    foreach (string line in lines) {
+                        sw.WriteLine(line.ToUpper());
+                    }
+                }
+            }
+            catch (IOException e) {
+                Console.WriteLine("An error ocurred");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
