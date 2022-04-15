95c59ee01d6e7cefa13b769b7b257c68247f467c
FernandoLuc4s <fernandolucas2002@gmail.com>
2022-04-15 11:11:48 -0300

    Directory, DirectoryInfo

---
+++ Seção 13 Trabalhando com arquivos/Trabalhando com arquivos/Aula_Directory_DirectoryInfo/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+
+namespace Aula_Directory_DirectoryInfo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\User\OneDrive\Área de Trabalho\udemy\C#\Seção 13 Trabalhando com arquivos\testes";
+
+            try
+            {
+               var folder = Directory.EnumerateDirectories(path, "*.*", SearchOption.AllDirectories); //"*.*" = qualquer nome de arquivo e qualquer extensão, resultado vai ser uma coleção de strings contendo as pastas
+               Console.WriteLine("FOLDERS:");
+               foreach (string s in folder) {
+                    Console.WriteLine(s); // imprime pastas no diretorio do path
+                }
+
+                var files = Directory.EnumerateFiles(path, "*.*", SearchOption.AllDirectories); //"*.*" = qualquer nome de arquivo e qualquer extensão, resultado vai ser uma coleção de strings contendo as pastas
+                Console.WriteLine("FILES:");
+                foreach (string s in files)
+                {
+                    Console.WriteLine(s); // imprime arquivos no diretorio do path
+                }
+
+                // criando uma pasta
+
+                Directory.CreateDirectory(path+ @"\newfolder");
+
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error ocurred");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
